7268c0c7ad024c5329a454a9f3653d4d0868edc1
josephlee01 <bluewings113@gmail.com>
2023-05-14 20:49:54 +1000

    1.3.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import { Routes, Route, HashRouter} from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import About from './pages/About';
 import Footer from './pages/Footer';
 import Home from './pages/Home';
 import Navbar from './pages/Navbar';
 import Projects from './pages/Projects';
 
-
-
 function App() {
   return (
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
-      <Navbar />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
       </div>
-        <Footer/>
+      <Footer />
     </HashRouter>
   );
 }
